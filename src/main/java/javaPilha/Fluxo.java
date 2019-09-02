c569005e43780b7ee374282951977ed910a980d2
Tayna Rodrigues <taynajesus@gmail.com>
2019-09-02 15:30:10 -0400

    Resolvendo exceções com Try Catch

--- src/main/java/javaPilha/Fluxo.java
+++ src/main/java/javaPilha/Fluxo.java
@@ -17,7 +17,23 @@ public class Fluxo {
 		System.out.println("Ini do metodo2");
 		for(int i =1; i <=5; i++) {
 			System.out.println(i);
-			int a = i / 0;
+			
+			/*
+			 * quando tentamos compilar int a = 1 / 0; /* Aparece ERRO NO CONSOLE Exception
+			 * in thread "main" java.lang.ArithmeticException: / by zero at
+			 * bytebankHerdado.TesteContas.main(TesteContas.java:8)
+			 */
+			//RESOLVENDO COM TRY CATCH
+			//try-> tentar 
+			//catch -> Palavra chave para capturar e pega essa exceção e executa o código
+			try {
+				int a = i / 0; // código perigoso!!!
+			} catch(ArithmeticException ex) {
+				System.out.println("ArithmeticException");
+			}
+			
+			
+		
 		}
 		System.out.println("Fim do metodo2");
 	}
